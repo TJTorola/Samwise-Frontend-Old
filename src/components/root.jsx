12f6a6b9fe83b648052367b7771ecb0064284a31
Tyler Torola <tyler@tjt.codes>
2016-11-18 16:37:39 -0800

    Adds react+redux skeleton

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
-export default () => <div>Are you sure you want to do this Tyler?</div>
+import App from './app';
+import configureStore from 'store/configure';
+
+const store = configureStore();
+window.store = store;
+
+export default () => (
+	<Provider store={ store }>
+		<App />
+	</Provider>
+);
---
+++ src/store/configure.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import Reducers from './reducers';
+import Middleware from './middleware';
+
+const configureStore = (preloadedState = {}) => createStore(
+	Reducers,
+	preloadedState,
+	Middleware
+);
+
+export default configureStore;
