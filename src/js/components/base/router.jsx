8757b7ecfaff9c09820c991df34cd3d3d29b2a02
Tyler Torola <tyler@tjt.codes>
2016-11-22 23:25:01 -0500

    adds basic router

---
+++ src/js/components/base/router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './app.jsx';
+import Login from 'components/pages/login';
+
+export default () => (
+	<Router history={ browserHistory }>
+		<Route path="/" component={ App } onEnter={ checkLogin }>
+		</Route>
+	</Router>
+);
