b19a831d268bbf58997795460433cb9eb116ecb4
Tyler Torola <tyler@tjt.codes>
2016-11-24 01:12:39 -0500

    fixed hot reloads

--- src/js/components/base/header/view.jsx
+++ src/js/components/base/header/view.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default () => (
 	<header className="avenir bg-blue">
-		<h1 className="bg-dark-blue white f3 normal ma0 mw4-ns tc-ns pa1"><strong>Admin P</strong>4x4</h1>
+		<h1 className="bg-dark-blue white f4 normal ma0 mw4-ns tc-ns pa1"><strong>Admin P</strong>4x4</h1>
 	</header>
 );
--- src/js/entry.jsx
+++ src/js/entry.jsx
@@ -5,5 +5,15 @@ import { AppContainer } from 'react-hot-loader';
 import Root from 'components/base/root';
 
 document.addEventListener("DOMContentLoaded", () => {
-	render(<Root />, document.querySelector('#root'));
+	render(<AppContainer><Root /></AppContainer>, document.querySelector('#root'));
 });
+
+if (module.hot) {
+	module.hot.accept('./components/base/root', () => {
+		let Root = require('components/base/root').default;
+		render(
+			<AppContainer><Root /></AppContainer>,
+			document.querySelector('#root')
+		);
+	});
+}
