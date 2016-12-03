f666a1d0a632f6b6b820980933bd25067ed217fb
Tyler Torola <tyler@tjt.codes>
2016-12-02 19:12:33 -0800

    moves modules and adds button spec and header spec todos

---
+++ test/components/base/header/main.spec.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import { Header } from 'components/base/header';
+
+test.todo('It is hidden when not logged in');
+
+test.todo('It has a toggle sidebar icon');
+
+test.todo('Icon is plus when sidebar is hidden');
+
+test.todo('Icon is minus when sidebar is shown');
+
+test.todo('Icon calls toggleNav when clicked');
---
+++ test/components/modules/button.spec.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import React from 'react';
+import { shallow } from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+import sinon from 'sinon';
+
+import { Button } from 'components/modules';
+
+test.todo('It passes in onClick');
+
+test.todo('It allows for multiple colors');
+
+test.todo('It displays given label');
